df9945b245e1da82453f594e37d90b02d80fcd53
Jairus Tanaka <me@jairus.dev>
2026-01-03 23:29:16 -0800

    chore: re-add js bench

--- run-bench.js.sh
+++ run-bench.js.sh
@@ -1,58 +1,34 @@
 #!/bin/bash
-set -euo pipefail
-
 RUNTIMES=${RUNTIMES:-"turbofan"}
-
 npx tsc -p ./bench > /dev/null 2>&1
 cp ./bench/lib/bench.js ./build/lib/bench.js
-
 mkdir -p ./build/logs/js
-mkdir -p ./build/logs/charts
-
-BENCH_NAME="${1:-}"
-FILES=()
-
-if [[ -n "$BENCH_NAME" ]]; then
-  # Allow `abc` or `abc.bench.ts`
-  [[ "$BENCH_NAME" != *.bench.ts ]] && BENCH_NAME="$BENCH_NAME.bench.ts"
-
-  CANDIDATE="./bench/$BENCH_NAME"
-  if [[ -f "$CANDIDATE" ]]; then
-    FILES+=("$CANDIDATE")
-  else
-    echo "❌ No JS benchmark found for '$1'"
-    exit 1
-  fi
-else
-  FILES=(./bench/*.bench.ts)
-fi
-
-for file in "${FILES[@]}"; do
+for file in ./bench/*.bench.ts; do
   filename=$(basename -- "$file")
   file_js="${filename%.ts}.js"
 
-  for rt in $RUNTIMES; do
-    runtime="${rt%%-*}"
-    engine="${rt#*-}"
+  output="./build/${filename%.ts}.wasm"
 
+  for rt in $RUNTIMES; do
+    runtime=$(echo $rt | cut -d'-' -f1)
+    engine=$(echo $rt | cut -d'-' -f2-)
     echo -e "$filename (js/$runtime/$engine)\n"
 
     arg="${filename%.ts}.${runtime}.ts"
-
     if [[ "$engine" == "ignition" ]]; then
-      v8 --no-opt --allow-natives-syntax --module "./build/$file_js" -- "$arg"
+      v8 --no-opt --allow-natives-syntax --module ./build/$file_js -- $arg
     fi
 
     if [[ "$engine" == "liftoff" ]]; then
-      v8 --liftoff-only --no-opt --allow-natives-syntax --module "./build/$file_js" -- "$arg"
+      v8 --liftoff-only --no-opt --allow-natives-syntax --module ./build/$file_js -- $arg
     fi
 
     if [[ "$engine" == "sparkplug" ]]; then
-      v8 --sparkplug --always-sparkplug --allow-natives-syntax --no-opt --module "./build/$file_js" -- "$arg"
+      v8 --sparkplug --always-sparkplug --allow-natives-syntax --no-opt --module ./build/$file_js -- $arg
     fi
 
     if [[ "$engine" == "turbofan" ]]; then
-      v8 --no-liftoff --no-wasm-tier-up --allow-natives-syntax --module "./build/$file_js" -- "$arg"
+      v8 --no-liftoff --no-wasm-tier-up --allow-natives-syntax --module ./build/$file_js -- $arg
     fi
   done
 done
