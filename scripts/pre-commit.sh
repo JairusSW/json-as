1ccb46dfef04d4c320481ee373b3630d2ee70797
Jairus Tanaka <me@jairus.dev>
2026-05-11 11:21:30 -0700

    chore: add husky precommit

---
+++ scripts/pre-commit.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+echo "Running formatter..."
+npm run format
+
+if ! git diff --quiet -- . ':(exclude)transform/lib'; then
+  echo
+  echo "Formatting changed files. Review, stage, and commit those changes before committing."
+  git --no-pager diff --stat -- . ':(exclude)transform/lib'
+  exit 1
+fi
+
+echo "Running linter..."
+npm run lint
+
+echo "Building transform..."
+npm run build:transform
